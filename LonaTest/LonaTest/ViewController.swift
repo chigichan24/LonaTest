68f7f60c3ea46433b5846e4c3208355b14d38806
Kazuki Chigita <kazuki.chigichan@gmail.com>
2020-02-17 19:33:24 +0900

    xcode project init

---
+++ LonaTest/LonaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LonaTest
+//
+//  Created by KazukiChigita on 2020/02/17.
+//  Copyright © 2020 chigichan24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
